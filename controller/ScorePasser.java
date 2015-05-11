fb9965255140b087f64348abc2d2feacb8862003
zzastrow2015 <zsz8804@gmail.com>
2015-05-11 00:10:35 -0500

    Update ScorePasser.java

--- controller/ScorePasser.java
+++ controller/ScorePasser.java
@@ -14,14 +14,15 @@ public class ScorePasser {
 	
 	public ScorePasser() throws IOException{
 		
+		//Splits the score, name, and scenario by tabs.
 		Scores topScores = new Scores(0, "");
 		ArrayList<String> top =  topScores.getScores();
 		String[] topSplit;
 		for(int i = 0; i < top.size(); i++){
 			topSplit = top.get(i).split("\t");
-			names.add(topSplit[0]);
-			scores.add(topSplit[1]);
-			scenarios.add(topSplit[2]);
+			names.add(topSplit[0]); //adds name
+			scores.add(topSplit[1]); //adds score
+			scenarios.add(topSplit[2]); //adds scenario
 		}
 	}
 	
